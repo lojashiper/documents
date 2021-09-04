bb5d029027a9f7757fbbeefba0a9eb50ef6d42be
lojashiper <89596426+lojashiper@users.noreply.github.com>
2021-09-04 14:57:47 -0300

    Update and rename main-v1.1.8.js to main-v1.1.9.js

--- main-v1.1.9.js
+++ main-v1.1.9.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
             document.querySelector('.main-product-buy-button-holder').insertAdjacentHTML('afterend', '<button id="button-retira-gratis" class="loader-button btn btn-primary" style=" width: 100%; background-color: var(--color-general-primary); border-color: var(--color-general-primary);">Retira Grátis <svg viewBox="0 0 36 8"><circle cx="4" cy="4" r="4"><animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin=".1"></animate></circle> <circle cx="18" cy="4" r="4"><animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin=".2"></animate></circle> <circle cx="32" cy="4" r="4"><animate attributeName="opacity" dur="1s" values="0;1;0" repeatCount="indefinite" begin=".3"></animate></circle></svg></button>');
             console.log('#botão de retirada grátis adicionado');
 
-            document.querySelector('#button-retira-gratis').insertAdjacentHTML('afterend', '<div class="modal-background"><div id="modal-retira-gratis" class="modal"><div class="flex -between"><div class="modal-header"><div class="theme-title">Retira Grátis</div> <div class="-subtitle"> Para o cep <span class="selected-retira-gratis"></span> <span class="-divisor"> | </span> <span class="selected-city"> - </span></div></div> <div class="close-modal"><i class="icon icon-close-modal"></i></div></div> <div class="modal-content"> <table class="table"><tr><th>Tipo</th> <th>Prazo</th> <th>Valor</th></tr> </table> <div class="disclaimer"> Prazo de entrega a partir da aprovação de pagamento <span class="display-hibrid">e envio ao operador logístico.</span></div></div></div></div></div>');
+            document.querySelector('#button-retira-gratis').insertAdjacentHTML('afterend', '<div class="modal-background retira-gratis"><div id="modal-retira-gratis" class="modal"><div class="flex -between"><div class="modal-header"><div class="theme-title">Retira Grátis</div> <div class="-subtitle"> Para o cep <span class="selected-retira-gratis"></span> <span class="-divisor"> | </span> <span class="selected-city"> - </span></div></div> <div class="close-modal"><i class="icon icon-close-modal"></i></div></div> <div class="modal-content"> <table class="table"><tr><th>Tipo</th> <th>Prazo</th> <th>Valor</th></tr> </table> <div class="disclaimer"> Prazo de entrega a partir da aprovação de pagamento <span class="display-hibrid">e envio ao operador logístico.</span></div></div></div></div></div>');
             console.log('#modal de retirada grátis adicionado');
             
             document.querySelector('#button-retira-gratis').onclick = function() {
@@ -13,6 +13,9 @@ document.addEventListener("DOMContentLoaded", function() {
             };
             document.querySelector('#modal-retira-gratis .close-modal').onclick = function() {
                 document.querySelector('#modal-retira-gratis').parentNode.classList.remove("active");
+            };
+            window.onclick = function(event) {
+                if(event.target.classList.contains('retira-gratis') && event.target.classList.contains('modal-background') && event.target.classList.contains('active')) event.target.classList.remove("active");
             };
             console.log('#scripts de retirada grátis adicionado');
         });
