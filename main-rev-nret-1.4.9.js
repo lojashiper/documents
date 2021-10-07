07492728b7b60f0af0162773d89f0d4b84fe26ff
lojashiper <89596426+lojashiper@users.noreply.github.com>
2021-10-07 16:29:58 -0300

    Update and rename main-rev-nret-1.4.8.js to main-rev-nret-1.4.9.js

--- main-rev-nret-1.4.9.js
+++ main-rev-nret-1.4.9.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function() {
             /* end:: Envia evento de adição ao carrinho */
             
             /* begin:: Informação do vendedor */
-            document.querySelector('.main-product-info .main-product-buy-button-holder').insertAdjacentHTML('afterend', '<p class="prod-current-seller" id="sold-by">Vendido por <span class="text-primary">Lojas Hiper</span></p>');
+            document.querySelector('.main-product-info .main-product-buy-button-holder').insertAdjacentHTML('afterend', '<p class="prod-current-seller" id="sold-by">Vendido e entregue por <span class="text-primary">Lojas Hiper</span></p>');
             console.log('#informação do vendedor adicionado');
             /* end:: Informação do vendedor */
             
