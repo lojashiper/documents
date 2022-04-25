5fa593c080fd0cd2f642657a9d57ed91363cfd81
lojashiper <89596426+lojashiper@users.noreply.github.com>
2022-04-25 00:13:24 -0300

    Update create-iframe-traking-1.0.js

--- create-iframe-traking-1.0.js
+++ create-iframe-traking-1.0.js
@@ -4,8 +4,8 @@ newIframe.src = 'about:blank';
 document.querySelector('#rastreio-yampi').appendChild(newIframe);
 
 var getHTML = '<div id="rastreio-yampi"></div>';
-var CSS = '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/lojashiper/documents/traking-1.2.css">';
-var JS = '<script src="//cdn.jsdelivr.net/gh/lojashiper/documents/traking-1.3.js"></script>';
+var CSS = '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/lojashiper/documents/traking-1.3.css">';
+var JS = '<script src="//cdn.jsdelivr.net/gh/lojashiper/documents/traking-1.4.js"></script>';
 var myContent = '<!DOCTYPE html>' + '<html><head><title>Rendered HTML from Pattern</title>' + CSS + '</head><body>' + getHTML + JS + '</body></html>';
 
 newIframe.contentWindow.document.open('text/html', 'replace');
