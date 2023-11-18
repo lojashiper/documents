918ba7450f26df7492257cd5e028676dc0ba9621
lojashiper <89596426+lojashiper@users.noreply.github.com>
2023-11-18 17:36:03 -0300

    Update and rename main-makro-1.2.0.js to main-makro-1.2.1.js

--- main-makro-1.2.1.js
+++ main-makro-1.2.1.js
@@ -323,50 +323,15 @@ document.onkeydown = function (e) {
     }
 }
 
-const devtools = {
-	isOpen: false,
-	orientation: undefined,
-};
-const threshold = 170;
-const emitEvent = (isOpen, orientation) => {
-	globalThis.dispatchEvent(new globalThis.CustomEvent('devtoolschange', {
-		detail: {
-			isOpen,
-			orientation,
-		},
+setInterval(() => {
+	console.log(Object.defineProperties(new Error, {
+		toString: { value() {(new Error).stack.includes('toString@') && alert('Safari devtools')}},
+		message: { get() {
+			document.body.innerHTML = '';
+		}},
 	}));
-};
+}, 1000);
 
-const main = ({emitEvents = true} = {}) => {
-	const widthThreshold = globalThis.outerWidth - globalThis.innerWidth > threshold;
-	const heightThreshold = globalThis.outerHeight - globalThis.innerHeight > threshold;
-	const orientation = widthThreshold ? 'vertical' : 'horizontal';
-
-	if (
-		!(heightThreshold && widthThreshold)
-		&& ((globalThis.Firebug && globalThis.Firebug.chrome && globalThis.Firebug.chrome.isInitialized) || widthThreshold || heightThreshold)
-	) {
-		if ((!devtools.isOpen || devtools.orientation !== orientation) && emitEvents) {
-			emitEvent(true, orientation);
-		}
-
-		devtools.isOpen = true;
-		devtools.orientation = orientation;
-	} else {
-		if (devtools.isOpen && emitEvents) {
-			emitEvent(false, undefined);
-		}
-
-		devtools.isOpen = false;
-		devtools.orientation = undefined;
-	}
-};
-
-main({emitEvents: false});
-setInterval(main, 500);
-window.addEventListener('devtoolschange', function (event) {
-    if(event.detail.isOpen) document.body.innerHTML = '';
-});
 /* end: Bloqueio de Segurança */
 
 const current_domain = window.location.hostname.replace('www.', '');
